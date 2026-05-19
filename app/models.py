aabd895a61faaed2ac591b31cc761026dd584fdb
kkb204 <kkb204@exeter.ac.uk>
2026-05-19 14:44:10 +0100

    Set up of database model.

--- app/models.py
+++ app/models.py
@@ -1,3 +1,11 @@
 from sqlalchemy import Column, Integer, String
 from app.database import Base
 
+class Workout(Base):
+    __tablename__ = "workouts"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String)
+    description = Column(String)
+    duration = Column(Integer)
+    date = Column(String)
